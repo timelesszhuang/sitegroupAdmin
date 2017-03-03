a4ba2b56a3b94fea74a3ed3f170a8045b62b88c2
2301887641 <15863549041@126.com>
2017-03-03 12:38:19 +0800

    提交

--- application/common/model/SiteUtil.php
+++ application/common/model/SiteUtil.php
@@ -11,7 +11,18 @@ use think\Session;
 
 class SiteUtil
 {
-
+    public function get_tree($data, $parent_id = 0, $level = 0) {
+        static $arr = array();
+        foreach ($data as $d) {
+            if ($d['parent_id'] == $parent_id) {
+                $d['level'] = $level;
+                $d['_parentId']=$d['parent_id'];
+                $arr[] = $d;
+                $this->get_tree($data, $d['id'], $level + 1);
+            }
+        }
+        return $arr;
+    }
 
 
 }
